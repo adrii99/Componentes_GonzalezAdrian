2cba852fdead624a8d8af93d454be98443095723
Adrian Gonzalez Garcia <adriangonzalezgarcia54@gmail.com>
2024-05-07 12:10:09 +0200

    temporizador terminado

--- src/main/java/es/ieslosmontecillos/componentes_gonzalezadrian/Temporizador.java
+++ src/main/java/es/ieslosmontecillos/componentes_gonzalezadrian/Temporizador.java
@@ -1,11 +1,17 @@
 package es.ieslosmontecillos.componentes_gonzalezadrian;
 
 import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -15,18 +21,21 @@ import java.io.IOException;
 
 public class Temporizador extends VBox {
 
-    @javafx.fxml.FXML
+    private IntegerProperty tiempo = new SimpleIntegerProperty();
+
+    //Creamos un objeto privado de un Action Event
+    private ObjectProperty<EventHandler<ActionEvent>> alTerminar
+            = new SimpleObjectProperty<EventHandler<ActionEvent>>();
+    @FXML
     private Label lbl;
+    private final Timeline timeline = new Timeline();
 
-    private Integer tiempo;
-
-    public Temporizador(Integer tiempo)
+    public Temporizador()
     {
-        this.setTiempo(tiempo);
-
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("temporizador.fxml"));
-        //fxmlLoader.setRoot(this);
+        fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
+
 
         try {
             fxmlLoader.load();
@@ -34,55 +43,45 @@ public class Temporizador extends VBox {
             throw new RuntimeException(exception);
         }
 
-        lbl.setText(tiempo.toString());
+       tiempo.addListener(new ChangeListener<Number>() {
+           @Override
+           public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
 
-        Timeline tl = new Timeline();
+               lbl.setText(String.valueOf(newValue));
 
-        tl.setCycleCount(getTiempo());
-
-        KeyFrame kf =new KeyFrame(Duration.seconds(1), e -> {
-            setTiempo(getTiempo()-1);
-            lbl.setText(getTiempo().toString());
-        });
-
-        tl.getKeyFrames().addAll(kf);
-
-        //Pasamos esta propiedad desde el constructor
-        tl.setOnFinished((ActionEvent event) -> {
-            onFinishedProperty().get().handle(event);
-        });
-
-        tl.play();
+           }
+       });
     }
 
-    //Constructor vacio
-    public Temporizador(){}
+    public Integer getTiempo(){return  tiempo.get();}
+
+    public void setTiempo(Integer t){tiempo.set(t);}
+
+    public IntegerProperty tiempoProperty(){return tiempo;}
 
 
-    public Integer getTiempo() {
-        return tiempo;
+    public void setAlTerminar(EventHandler<ActionEvent> handler) {
+        alTerminar.set(handler);
     }
 
-    public void setTiempo(Integer tiempo) {
-        this.tiempo = tiempo;
+    public  EventHandler<ActionEvent> getAlTerminar() {
+        return alTerminar.get();
     }
 
-    //Creamos un objeto privado de un Action Event
-    private ObjectProperty<EventHandler<ActionEvent>> propertyOnFinished
-            = new SimpleObjectProperty<EventHandler<ActionEvent>>();
-
-    //Creamos un metodo para recuperar ese objeto privado de tipo object property action event
-    public final ObjectProperty<EventHandler<ActionEvent>> onFinishedProperty() {
-        return propertyOnFinished;
+    public  ObjectProperty<EventHandler<ActionEvent>> alTerminarProperty() {
+        return alTerminar;
     }
 
-    //Creamos un setter de esta propiedad
-    public final void setOnFinished(EventHandler<ActionEvent> handler) {
-        propertyOnFinished.set(handler);
+    public void iniciarTemporizador()
+    {
+
+        final KeyValue kv = new KeyValue(tiempo, 0);
+        final KeyFrame kf = new KeyFrame(Duration.seconds(getTiempo()),getAlTerminar(),kv);
+
+        lbl.setText(String.valueOf(getTiempo()));
+
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
     }
 
-    //Creamos un getter
-    public final EventHandler<ActionEvent> getOnFinished() {
-        return propertyOnFinished.get();
-    }
 }
--- src/main/java/es/ieslosmontecillos/componentes_gonzalezadrian/TemporizadorEjemplo.java
+++ src/main/java/es/ieslosmontecillos/componentes_gonzalezadrian/TemporizadorEjemplo.java
@@ -1,31 +1,39 @@
 package es.ieslosmontecillos.componentes_gonzalezadrian;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class TemporizadorEjemplo extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        //Crea una instancia de Temporizador con un tiempo
-        Temporizador temp = new Temporizador(5);
+        //Crea una instancia de Temporizador
+        Temporizador temp = new Temporizador();
+        Label lbl = new Label();
 
         VBox root = new VBox();
-        System.out.println(temp.getTiempo());
+        root.getChildren().addAll(temp,lbl);
+
+        temp.setTiempo(10);
+
+        temp.setAlTerminar(e -> {
+            lbl.setText("Se acabo el tiempo");
+        });
+
+        temp.iniciarTemporizador();
 
         Scene scene = new Scene(root, 600, 400);
 
         stage.setScene(scene);
         stage.setTitle("Temporizador");
         stage.show();
-
-        temp.setOnFinished(e -> {
-            Label lbl = new Label("Se acabo el tiempo!!");
-            root.getChildren().add(lbl);
-        });
 
     }
     public static void main(String[] args) {
